a1f9c2d4d40766211e653a15d64b4fc5e5885f06
Sébastien Weber <seba.weber@gmail.com>
2023-02-10 17:25:04 +0100

    plugin is working!!

--- src/pymodaq_plugins_crystal_technology/hardware/aods_controller.py
+++ src/pymodaq_plugins_crystal_technology/hardware/aods_controller.py
@@ -6,29 +6,31 @@ import time
 import ctypes
 from typing import Tuple
 import portion
-
 import toml
-
+import numpy as np
 from numpy.polynomial import Polynomial
+
+from pymodaq.daq_utils.daq_utils import set_logger
 
 
 path = Path(r'C:\Program Files\Crystal Technology\Developer\AotfLibrary\Dll')
 os.add_dll_directory(str(path))
-
 dll = ctypes.cdll.LoadLibrary('AotfLibrary.dll')
+calib_path = Path(__file__).parent
+calibration = toml.load(calib_path.joinpath('calibration.toml'))
+calib_ids = list(calibration.keys())
+logger = set_logger('AOTF', add_to_console=True)
 
-calibration = toml.load('./calibration.toml')
 
 class AOTF:
-
     def __init__(self):
         super().__init__()
         self._handle: int = None
         self._nchannels = 8
         self._calibration: Polynomial = None
-        self.calib_ids = list(calibration.keys())
+        self.calib_ids = calib_ids
         self._timeout = 10  # seconds
-        self._amplitude_int_max = 16383
+        self._current_channel: Channel = None
 
     def _check_handle(self):
         """make sure the communication with the controller has been opened"""
@@ -36,6 +38,7 @@ class AOTF:
             raise IOError('The communication with the controller has not been opened, or failed')
 
     def _write(self, message: str):
+        logger.debug(f'Writing: {message} to the controller')
         self._check_handle()
         buffer = ctypes.create_string_buffer(message.encode())
         ret = dll.AotfWrite(self._handle, len(message), ctypes.byref(buffer))
@@ -50,7 +53,9 @@ class AOTF:
             ret = dll.AotfRead(self._handle, length, ctypes.byref(buffer), ctypes.byref(nread))
             if ret == 0:
                 raise IOError(f'The program could not send data to the controller')
-            return buffer.value[:nread.value].decode()
+            read = buffer.value[:nread.value].decode()
+            logger.debug(f'Reading: {read} from the controller')
+            return read
         else:
             return ''
 
@@ -79,6 +84,7 @@ class AOTF:
                 return ''
 
     def _check_message_done(self, read: str, msg: str):
+        #print(read)
         splits = read.split('\r\n')
         if splits[0] != msg:
             return False
@@ -86,8 +92,11 @@ class AOTF:
             return '*' in splits[-1]
 
     def query(self, msg: str):
+        logger.debug(f'Querying: {msg} from the controller')
         self._write(f'{msg}\r')
-        return self._loop_read(msg)
+        response = self._loop_read(msg)
+        logger.debug(f'Query answer is: {response} from the controller')
+        return response
 
     def write(self, msg: str):
         print(msg)
@@ -118,21 +127,7 @@ class AOTF:
         return self.query('BoardId Date')
 
     def reset(self):
-        self.write(f'dds reset')
-
-    def set_acoustic_frequency(self, frequency: float, channel: int = 0) -> float:
-        """Get/Set the acoustic frequency in Hz"""
-        return self.set_acoustic_frequency_Hz(frequency, channel)
-
-    def set_acoustic_frequency_MHz(self, frequency: float, channel: int = 0) -> float:
-        """Get/Set the acoustic frequency in MHz"""
-        self._check_channel(channel)
-        self.write(f'Dds Frequency {channel} {frequency}')
-
-    def set_acoustic_frequency_Hz(self, frequency: float, channel: int = 0) -> float:
-        """Get/Set the acoustic frequency in Hz"""
-        self._check_channel(channel)
-        self.write(f'Dds Frequency {channel} !{frequency}')
+        self.query(f'dds reset')
 
     @property
     def calibration(self) -> Polynomial:
@@ -145,45 +140,111 @@ class AOTF:
                                            domain=calibration[calibration_id]['domain'],
                                            window=calibration[calibration_id]['domain'])
 
-    def set_wavelength(self, wavelength: float, channel: int = 0):
+    def get_channel(self, channel: int) -> 'Channel':
         self._check_channel(channel)
-        if self._calibration is not None and wavelength in portion.closed(*self._calibration.domain):
-            self.set_acoustic_frequency_MHz(self.calibration(wavelength), channel)
+        self._current_channel = Channel(channel, self)
+        return self._current_channel
 
-    def set_amplitude_int(self, amplitude: int, channel: int = 0):
-        self._check_channel(channel)
-        if amplitude in portion.closed(0, self._amplitude_int_max):
-            self.write(f'Dds Amplitude {channel} {amplitude}')
 
-    def set_amplitude_percent(self, amplitude: float, channel: int = 0):
-        amplitude_int = self._amplitude_int_max * amplitude / 100
-        self.set_amplitude_int(amplitude_int, channel)
+class Channel:
+    def __init__(self, channel: int, aotf: AOTF = None):
+        self._channel = channel
+        self._aotf = aotf
+        self._amplitude_int_max = 16383
 
-    def set_amplitude(self, amplitude: float, channel: int = 0):
-        self.set_amplitude_percent(amplitude, channel)
+    def __repr__(self):
+        return f'Channel {self._channel} from {self._aotf}'
 
+    @property
+    def acoustic_frequency_MHz(self) -> float:
+        """Get/Set the acoustic frequency in MHz"""
+        #Channel 0 profile 0 frequency
+        response = self._aotf.query(f'dds frequency {self._channel}')
+        if f'Channel {self._channel} profile 0 frequency ' in response:
+            response = response.split(f'Channel {self._channel} profile 0 frequency ')[1]
+            response = response.split('Hz')[0]
+            afreq = float(response)
+            return afreq * 1e-6
+
+    @acoustic_frequency_MHz.setter
+    def acoustic_frequency_MHz(self, frequency: float):
+        """Get/Set the acoustic frequency in MHz"""
+        self._aotf.query(f'Dds Frequency {self._channel} {frequency}')
+
+    @property
+    def wavelength(self) -> float:
+        acoustic_frequency_MHz = self.acoustic_frequency_MHz
+        if not np.isclose(acoustic_frequency_MHz, 0.0):
+            roots = (self._aotf.calibration-acoustic_frequency_MHz).roots()
+            for root in roots:
+                if np.isclose(root, np.abs(root)) and np.real(root) in portion.closed(*self._aotf.calibration.domain):
+                    return np.real(root)
+        else:
+            return 0.
+
+    @wavelength.setter
+    def wavelength(self, wavelength: float):
+        if self._aotf.calibration is not None and wavelength in portion.closed(*self._aotf.calibration.domain):
+            self.acoustic_frequency_MHz = self._aotf.calibration(wavelength)
+
+    @property
+    def amplitude_int(self) -> int:
+        """Get/Set the current channel amplitude in percent"""
+        amplitude_str = self._aotf.query(f'dds amplitude {self._channel}')
+        return int(amplitude_str.split(f'Channel {self._channel} @ ')[1])
+
+    @amplitude_int.setter
+    def amplitude_int(self, amp: int):
+        if amp in portion.closed(0, self._amplitude_int_max):
+            self._aotf.query(f'Dds Amplitude {self._channel} {amp}')
+
+    @property
+    def amplitude(self) -> float:
+        """Get/Set the current channel amplitude in percent"""
+        amplitude_int = self.amplitude_int
+        return amplitude_int * 100 / self._amplitude_int_max
+
+    @amplitude.setter
+    def amplitude(self, amp: float):
+        self.amplitude_int = int(amp * self._amplitude_int_max / 100)
 
 
 if __name__ == '__main__':
     aotf = AOTF()
     try:
         aotf.open(0)
-        print(aotf.get_serial())
-        print(aotf.get_date())
+        print(f'Serial is: {aotf.get_serial()}')
+        print(f'Date is: {aotf.get_date()}')
+        print("Reseting")
         aotf.reset()
         calib_ids = aotf.calib_ids
-        channel = 0
+        channel_int = 0
         if 'RF1' in calib_ids:
+            print("Setting calibration")
             aotf.calibration = 'RF1'
-            aotf.set_wavelength(532., channel)
-            aotf.set_amplitude_int(8000, 0)
-            aotf.set_amplitude(0, 0)
-            aotf.set_amplitude(60, 0)
-            aotf.set_amplitude(0, 0)
+            channel = aotf.get_channel(channel_int)
+            print(f'Selecting channel: {channel}')
+            print(f'Wavelength is {channel.wavelength}')
+            print(f'Setting wavelength to 532nm')
+            channel.wavelength = 532.
+            print(f'Wavelength is {channel.wavelength}')
+            print(f'Amplitude is {channel.amplitude}')
+            print(f'Setting amplitude int to 8000')
+            channel.amplitude_int = 8000
+            print(f'Amplitude int is {channel.amplitude_int}')
+            print(f'Setting amplitude to 0')
+            channel.amplitude = 0
+            print(f'Amplitude is {channel.amplitude}')
+            print(f'Setting amplitude to 60%')
+            channel.amplitude = 60
+            print(f'Amplitude is {channel.amplitude}%')
+            print(f'Setting amplitude to 0')
+            channel.amplitude = 0
 
     except Exception as e:
         print(str(e))
     finally:
+        print(f'Close and quit')
         aotf.close()
 
 
